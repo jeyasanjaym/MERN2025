fbdebb9b270a77d549a1757b4f118150915077e2
jeyasanjaym <sanjaymjs789@gmail.com>
2025-06-20 08:54:19 +0530

    Initial commit

--- react_project/src/App.jsx
+++ react_project/src/App.jsx
@@ -1,22 +1,26 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React from 'react'
+// import { Child } from './pages/Child'
+import Child1 from './components/Child1'
+import Child2 from './components/child2'
+import Pages from './pages/pages'
+import About from './pages/about'
+import Contact from './pages/contact'
+import Skills from './pages/skills'
 
-function App() {
-  const [count, setCount] = useState(0)
-
+const App = () => {
+  var arr = ['hai','hello','print'];
+  var user = {username :'sanjaymjs',password : "123456"};
   return (
-    <>
     <div>
-      <h1>JEYASANJAY</h1>
+      <Child1 name="sanjay" phonenum="9878909878"/>
+      <Child2/>
+      <Pages items={arr} users = {user}/>
+      <About/>
+      <Contact/>
+      
+
     </div>
-        <div>
-      <h1></h1>
-    </div>
-    </>
   )
 }
-
 
 export default App
